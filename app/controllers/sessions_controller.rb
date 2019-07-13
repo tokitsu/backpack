e65700a3ecabb3e4195472bb7c59662b18063584
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-07-14 05:36:53 +0900

    add default image in user profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      render 'home/index'
+      render 'lohin_path'
     end
   end
 
