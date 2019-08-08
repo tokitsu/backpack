08e59e08520a92cea4f9d007294d46d02aad3163
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-08-08 22:29:40 +0900

    adjusted ajax in favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,12 +9,8 @@ class FavoritesController < ApplicationController
 
   def create
     user = current_user
-    board = Board.find(params[:board_id])
-    if Favorite.create(user_id: user.id, board_id: board.id)
-      @boards = Board.all
-    respond_to do |format|
-      format.js {render :template => "boards/index",locals: { favorite: @favorite, board: @board} }
-    end
+    @board = Board.find(params[:board_id])
+    if Favorite.create(user_id: user.id, board_id: @board.id)
     else
       flash[:notice] = '登録できませんでした。'
       redirect_to boards_path
@@ -23,13 +19,9 @@ class FavoritesController < ApplicationController
 
   def destroy
     user = current_user
-    board = Board.find(params[:board_id])
-    if favorite = Favorite.find_by(user_id: user.id,board_id: board.id)
-      favorite.destroy
-      @boards = Board.all
-    respond_to do |format|
-      format.js {render :template => "boards/index",locals: { favorite: @favorite, board: @board} }
-    end
+    @board = Board.find(params[:board_id])
+    favorite = Favorite.find_by(user_id: user.id, board_id: @board.id)
+    if favorite.destroy
     else
       flash[:notice] = 'お気に入り解除できませんでした。'
       redirect_to boards_path
