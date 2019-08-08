fcd316c38ef6fa2d9c047c3b6d7bb2c8fd69ace1
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-08-08 19:28:11 +0900

    add ajax in favorite function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery3
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,8 +11,10 @@ class FavoritesController < ApplicationController
     user = current_user
     board = Board.find(params[:board_id])
     if Favorite.create(user_id: user.id, board_id: board.id)
-      @favorites = Favorite.where(board_id: params[:board_id])
       @boards = Board.all
+    respond_to do |format|
+      format.js {render :template => "boards/index",locals: { favorite: @favorite, board: @board} }
+    end
     else
       flash[:notice] = '登録できませんでした。'
       redirect_to boards_path
@@ -24,8 +26,10 @@ class FavoritesController < ApplicationController
     board = Board.find(params[:board_id])
     if favorite = Favorite.find_by(user_id: user.id,board_id: board.id)
       favorite.destroy
-      @favorites = Favorite.where(board_id: params[:board_id])
       @boards = Board.all
+    respond_to do |format|
+      format.js {render :template => "boards/index",locals: { favorite: @favorite, board: @board} }
+    end
     else
       flash[:notice] = 'お気に入り解除できませんでした。'
       redirect_to boards_path
