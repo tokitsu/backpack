e0376d1985d5ec5ae2787d94e5fa0bf04de2a8bd
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-08-29 18:50:57 +0900

    adjusted image size in board/index

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id              :integer          not null, primary key
+#  body            :text(65535)
+#  city            :string(255)
+#  country         :string(255)
+#  favorites_count :integer
+#  title           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  comment    :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_board_id  (board_id)
+#  index_comments_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
