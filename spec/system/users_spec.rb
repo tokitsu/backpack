0feaf5a1284e17d2c245fcb778a2b7ca5a29a35b
時津尉真 <togitsujoushin@togitsujoushinnoMacBook-Pro.local>
2019-09-06 21:36:10 +0900

    add system spec in users_spec.rb

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'ユーザー機能', type: :system do
 
+  let(:user_a) {FactoryBot.create(:user, name: 'userA', email: 'a@mail.com')}
+
   describe '新規ユーザー作成機能' do
 
     before do
@@ -9,79 +11,69 @@ describe 'ユーザー機能', type: :system do
     end
 
     it '新規ユーザー作成が成功する' do
-      fill_in 'user[name]', with 'userA'
-      fill_in 'user[email]', with 'example@email.com'
-      fill_in 'user[password]',with 'password'
-      fill_in 'user[password_confirmation]', with 'password'
-      click_button ''
+      fill_in 'user[name]', with: 'userA'
+      fill_in 'user[email]', with: 'example@email.com'
+      fill_in 'user[password]',with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button '新規作成'
+
+      expect(page).to have_content 'ユーザー登録が完了しました！！'
+    end
+
+    it 'ユーザー名が空のため、ユーザー作成が失敗する' do
+      fill_in 'user[name]', with: ''
+      fill_in 'user[email]', with: 'example@email.com'
+      fill_in 'user[password]',with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button '新規作成'
+
+      expect(page).to have_content 'ユーザー名を入力してください'
+    end
+
+    it 'メールアドレスが空のため、ユーザー作成が失敗する' do
+      fill_in 'user[name]', with: 'userA'
+      fill_in 'user[email]', with: ''
+      fill_in 'user[password]',with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button '新規作成'
+
+      expect(page).to have_content 'メールアドレスを入力してください'
+    end
+
+    it 'パスワードとパスワードコンファメーションが一致しないため、ユーザー作成が失敗する' do
+      fill_in 'user[name]', with: 'userA'
+      fill_in 'user[email]', with: 'example@email.com'
+      fill_in 'user[password]',with: 'ppssword'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button '新規作成'
+
+      expect(page).to have_content 'パスワード（確認）とパスワードの入力が一致しません'
+    end
+
+    it 'パスワードが８文字以下の場合、ユーザー作成に失敗する' do
+      fill_in 'user[name]', with: 'userA'
+      fill_in 'user[email]', with: 'example@email.com'
+      fill_in 'user[password]',with: 'pass'
+      fill_in 'user[password_confirmation]', with: 'pass'
+      click_button '新規作成'
+
+      expect(page).to have_content 'パスワードは8文字以上で入力してください'
+    end
+  end
+
+  describe 'ログイン機能' do
+
+
+    before do
+      visit login_path
+    end
+
+    it 'ログインが成功する。' do
+      fill_in 'session[email]', with: login_user.email
+      fill_in 'session[password]', with: login_user.password
+      click_button 'ログイン'
+
+      expect(page).to have_content 'ログインしました！'
     end
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-require 'rails_helper'
-
-describe 'ノート管理機能', type: :system do
-
-  let(:user_a) {FactoryBot.create(:user, name: 'userA', email: 'a@mail.com')}
-  let(:user_b) {FactoryBot.create(:user, name: 'userB', email: 'b@mail.com')}
-  let!(:board_a) {FactoryBot.create(:board, title: 'ノートA', body: '最初の投稿', user:user_a)}
-
-  before do
-    visit login_path
-    fill_in 'session[email]', with: login_user.email
-    fill_in 'session[password]', with: login_user.password
-    click_button 'ログイン'
-  end
-
-  describe '新規作成' do
-    context 'ユーザーAがログインしているとき' do
-      let(:login_user) {user_a}
-
-      before do
-        visit new_board_path
-      end
-
-      it '新規投稿が成功する' do
-
-        fill_in 'board[title]', with: 'ノートB'
-        fill_in 'board[body]', with: 'ノートBの内容'
-        click_button '投稿する'
-
-        expect(page).to have_content 'ノートが作成されました'
-
-      end
-
-      it 'タイトルが空欄の場合、投稿に失敗する' do
-        fill_in 'board[title]', with: ''
-        fill_in 'board[body]', with: 'ノートBの内容'
-        click_button '投稿する'
-
-        expect(page).to have_content 'タイトルを入力してください'
-      end
-
-      it '本文が空欄の場合、投稿に失敗する' do
-        fill_in 'board[title]', with: 'ノートB'
-        fill_in 'board[body]', with: ''
-        click_button '投稿する'
-
-        expect(page).to have_content '本文を入力してください'
-      end
-    end
-  end
